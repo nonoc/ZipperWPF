44be352fdcd1e4e30625c65a818aac733b063fc7
newland <juanantonio_calero@hotmail.com>
2017-07-11 14:53:01 +0200

    Agregar archivos de proyecto.

---
+++ ZipperWPF/App.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ZipperWPF
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        string fileLog;
+
+        void app_Startup(object sender, StartupEventArgs e)
+        {
+            // If no command line arguments were provided, don't process them 
+            if (e.Args.Length == 0) return;
+
+            if (e.Args.Length > 0)
+            {
+                fileLog = e.Args[0] + @"\ko.log";
+                StreamWriter sw = new StreamWriter(fileLog, false);
+                try
+                {
+                    // Open the text file using a stream reader. 
+                    //using (StreamReader sr = new StreamReader(args[0]))
+                    //{
+                    // Read the stream to a string, and write  
+                    // the string to the text box 
+                    //String line = sr.ReadToEnd();
+                    //textBox.AppendText(line.ToString());
+                    //textBox.AppendText("\n");                    
+                    sw.WriteLine("# Informaion de parametros de entrada: " + fileLog);
+                    
+                    //}
+                }
+                catch (Exception error)
+                {
+                    //textBox.AppendText("The file could not be read:");
+                    //textBox.AppendText("\n");
+                    //textBox.AppendText(e.Message);
+                    sw.WriteLine("# Informaion de parametros de error: " + error.Message);
+                }
+                sw.Close();
+
+                Application.Current.Shutdown();
+            }
+        }
+
+    }
+}
---
+++ ZipperWPF/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.IO.Compression;
+using Microsoft.Win32;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace ZipperWPF
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        string startPath = @"c:\example\start";
+        string zipPath = @"c:\example\result.zip";
+        string extractPath = @"c:\example\extract";        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnComprimir_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ZipFile.CreateFromDirectory(lblPathOrigen.Content.ToString(), zipPath);
+                
+            }
+            catch (Exception ex)
+            {
+                lblMsgError.Content = ex.Message.ToString();                
+            }
+
+        }
+
+
+        
+
+        private void BtnExtraer_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ZipFile.ExtractToDirectory(zipPath, lblPathDestino.Content.ToString());
+            }
+            catch (Exception ex)
+            {
+                lblMsgError.Content = ex.Message.ToString();
+            }
+           
+        }
+
+      
+
+        private void BtnRutaOrigen_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            folderDialog.ShowNewFolderButton = false;
+            folderDialog.SelectedPath = System.AppDomain.CurrentDomain.BaseDirectory;
+
+            if (ConfigurationManager.AppSettings["PrevPathOrigen"].ToString() != "")
+                folderDialog.SelectedPath = ConfigurationManager.AppSettings["PrevPathOrigen"].ToString();
+
+            if (folderDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                lblPathOrigen.Content = folderDialog.SelectedPath;
+                ConfigurationManager.AppSettings["PrevPathOrigen"] = folderDialog.SelectedPath;
+            }
+        }
+
+        private void BtnRutaDestino_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            folderDialog.ShowNewFolderButton = false;
+            folderDialog.SelectedPath = System.AppDomain.CurrentDomain.BaseDirectory;
+
+            if (folderDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                lblPathDestino.Content = folderDialog.SelectedPath;
+            }
+
+        }
+        private void ClearMsgError()
+        {
+            lblPathDestino.Content = "";
+        }
+    }
+}
